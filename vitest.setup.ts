a60ada920c59ea886c971552c712334f210e8074
Gaurav <gauravramyadav@gmail.com>
2026-08-14 02:00:24 +0530

    idk

--- vitest.setup.ts
+++
@@ -1,5 +0,0 @@
-import "dotenv/config";
-
-process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT ??= "https://cloud.appwrite.io/v1";
-process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID ??= "test-project";
-process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID ??= "test-database";
